6f0333a27133422f82477b8c98585a0143665cd1
IT25103235 <it25103235@my.sliit.lk>
2026-05-17 15:33:57 +0530

    Update feedback file

--- src/main/java/com/example/car_service/feedback/Feedback.java
+++ src/main/java/com/example/car_service/feedback/Feedback.java
@@ -20,26 +20,22 @@ public class Feedback {
     @Column(name = "id")
     private Long id;
 
-    //ID of the related service booking
+
     @Column(name = "booking_id", nullable = false)
     private Long bookingId;
 
-    //ID of the customer who submitted the feedback
     @Column(name = "customer_id", nullable = false)
     private Long customerId;
 
-    //Customer rating value(1 to 5)
     @Column(name = "rating", nullable = false)
     private Integer rating;
 
-    //Additional review comment provided by the customer
     @Column(name = "comment", columnDefinition = "TEXT")
     private String comment;
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    //Automatically sets the creation time before inserting the record.
     @PrePersist
     protected void onCreate() {
 
