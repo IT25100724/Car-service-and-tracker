ab18abe92ebf74e74f7b7e0f716fd1f9413bf0bc
IT25100724 <it25100724@my.sliit.lk>
2026-05-19 16:18:56 +0530

    Update Customer file

--- src/main/java/com/example/car_service/customer/Customer.java
+++ src/main/java/com/example/car_service/customer/Customer.java
@@ -2,30 +2,76 @@ package com.example.car_service.customer;
 
 import com.example.car_service.user.BaseEntity;
 import jakarta.persistence.*;
-import lombok.*;
 
+// Represents a customer profile in the system
 @Entity
 @Table(name = "customers")
-@Data
-@EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-// INHERITANCE: Customer extends BaseEntity to inherit the 'id' and 'createdAt' fields.
+
+// Inheritance form BaseEntity
 public class Customer extends BaseEntity {
 
-    @Column(name = "first_name", nullable = false)
+
+    // Encapsulation (Attribute as Private )
     private String firstName;
-
-    @Column(name = "last_name", nullable = false)
     private String lastName;
-
-    @Column(name = "email", nullable = false, unique = true)
     private String email;
-
-    @Column(name = "phone")
     private String phone;
-
-    @Column(name = "address")
     private String address;
+
+
+    // Default constructor
+    public Customer() {
+    }
+
+
+    // Parameterize Constructor (DTO)
+    public Customer(String firstName, String lastName, String email, String phone, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+
+    // Encapsulation (Below using getter/setter)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
